2bc4d39cce4ab85ccfd4c76974e67b68d069756b
Phuc Ho <hotrongphuc643@gmail.com>
2024-07-08 13:05:02 +0700

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
-import { logo } from "../utils/constants";
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+  <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
+      <span style={{ color: "red", display: "flex", alignItems: "center" }}>
+        <YouTubeIcon style={{ fontSize: 60 }} />
+        <span style={{ fontSize: 30, color: "white", fontFamily: "fantasy" }}>Youtabe</span>
+      </span>
     </Link>
     <SearchBar />
   </Stack>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,32 +11,34 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import TheatersIcon from '@mui/icons-material/Theaters';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
-  { name: 'ReactJS', icon: <CodeIcon />, },
-  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Short', icon: <TheatersIcon />, },
+  // { name: 'ReactJS', icon: <CodeIcon />, },
+  // { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
   { name: 'Movie', icon: <OndemandVideoIcon />, },
   { name: 'Gaming', icon: <SportsEsportsIcon />, },
   { name: 'Live', icon: <LiveTvIcon />, },
-  { name: 'Sport', icon: <FitnessCenterIcon />, },
-  { name: 'Fashion', icon: <CheckroomIcon />, },
-  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
-  { name: 'Comedy', icon: <TheaterComedyIcon />, },
-  { name: 'Gym', icon: <FitnessCenterIcon />, },
-  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+  { name: 'Sport', icon: <SportsSoccerIcon />, },
+  // { name: 'Fashion', icon: <CheckroomIcon />, },
+  // { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  // { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  // { name: 'Gym', icon: <FitnessCenterIcon />, },
+  // { name: 'Crypto', icon: <DeveloperModeIcon />, },
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoThumbnailUrl = 'https://cdn.vox-cdn.com/thumbor/6bvYA8Z7hBRrMJ965Rb20uKB628=/0x0:1192x795/1200x628/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg';
+export const demoChannelUrl = '/channel/UCY5Zgu_X7DNXb9bUFsQPXTQ';
+export const demoVideoUrl = '/video/dQw4w9WgXcQ';
+export const demoChannelTitle = 'My channel';
+export const demoVideoTitle = 'Never Gonna Give You Up';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
